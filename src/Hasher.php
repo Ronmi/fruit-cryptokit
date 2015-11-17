e748814f44aa638196fa69183fdca0dd40581197
Ronmi Ren <ronmi.ren@gmail.com>
2015-11-17 11:36:21 +0800

    update api format

--- src/Hasher.php
+++ src/Hasher.php
@@ -2,9 +2,22 @@
 
 namespace Fruit\CryptoKit;
 
+/**
+ * Define api interface to compute hashsum.
+ *
+ * The design is roughly copy of php hash extension.
+ */
 interface Hasher
 {
-    public function sum($data);
+    /**
+     * update() and updateStream() allocates a context if needed,
+     * and pump data into the context.
+     */
+    public function update($data);
+    public function updateStream($handle);
 
-    public function sumStream($handle);
+    /**
+     * Get computed hashsum and free the context.
+     */
+    public function sum($raw = false);
 }
