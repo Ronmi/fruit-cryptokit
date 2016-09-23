b5e5bc23137b6aeeaa5f705ff144bc8a8854a730
Ronmi Ren <ronmi.ren@gmail.com>
2016-09-23 11:25:35 +0800

    move static array outside, boost performance 100%

--- src/Base32.php
+++ src/Base32.php
@@ -99,20 +99,27 @@ class Base32 implements Crypter
         return self::doDecrypt($this->invertMap, $data);
     }
 
+    private static $mapStd = array(
+        // hard-coded here, make it run faster in self::D()
+        'A' =>  0, 'B' =>  1, 'C' =>  2, 'D' =>  3,
+        'E' =>  4, 'F' =>  5, 'G' =>  6, 'H' =>  7,
+        'I' =>  8, 'J' =>  9, 'K' => 10, 'L' => 11,
+        'M' => 12, 'N' => 13, 'O' => 14, 'P' => 15,
+        'Q' => 16, 'R' => 17, 'S' => 18, 'T' => 19,
+        'U' => 20, 'V' => 21, 'W' => 22, 'X' => 23,
+        'Y' => 24, 'Z' => 25, '2' => 26, '3' => 27,
+        '4' => 28, '5' => 29, '6' => 30, '7' => 31,
+        // replicate for lowercase
+        'a' =>  0, 'b' =>  1, 'c' =>  2, 'd' =>  3,
+        'e' =>  4, 'f' =>  5, 'g' =>  6, 'h' =>  7,
+        'i' =>  8, 'j' =>  9, 'k' => 10, 'l' => 11,
+        'm' => 12, 'n' => 13, 'o' => 14, 'p' => 15,
+        'q' => 16, 'r' => 17, 's' => 18, 't' => 19,
+        'u' => 20, 'v' => 21, 'w' => 22, 'x' => 23,
+        'y' => 24, 'z' => 25,
+    );
     public static function D($data)
     {
-        static $map = array(
-            // hard-coded here, make it run faster
-            'A' =>  0, 'B' =>  1, 'C' =>  2, 'D' =>  3,
-            'E' =>  4, 'F' =>  5, 'G' =>  6, 'H' =>  7,
-            'I' =>  8, 'J' =>  9, 'K' => 10, 'L' => 11,
-            'M' => 12, 'N' => 13, 'O' => 14, 'P' => 15,
-            'Q' => 16, 'R' => 17, 'S' => 18, 'T' => 19,
-            'U' => 20, 'V' => 21, 'W' => 22, 'X' => 23,
-            'Y' => 24, 'Z' => 25, '2' => 26, '3' => 27,
-            '4' => 28, '5' => 29, '6' => 30, '7' => 31,
-        );
-
         return self::doDecrypt(self::$map, $data);
     }
 
