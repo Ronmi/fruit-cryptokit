81b75e462b816bbdedf5d89926b3875f12cb5d0f
Ronmi Ren <ronmi.ren@gmail.com>
2016-09-23 10:41:36 +0800

    add more benchmark for base32

--- bench/base32.php
+++ bench/base32.php
@@ -30,6 +30,14 @@ class Base32Bench
             $this->b->encrypt('123456');
         }
     }
+    public function Encrypt30cm(Benchmark $b)
+    {
+        $data = 'SeGVUZ9mkK6tUvn727dinTDlUcw9B5WqwyAQLAOmHm78dQDtU9BZuqsroNuBYZI6bsM/Fh3zgS7Itdf9d7eHyI1i8aEwI9gFjbacCpBW2b1TRd7yTEErIzM+9m+BMUDy9jpJHBqOCtizAUGcEW7vxDeKqKax+kDA';
+        for ($i = 0; $i < $b->n; $i++) {
+            $this->b->encrypt($data);
+        }
+    }
+
     public function DecryptLess(Benchmark $b)
     {
         $code = $this->b->encrypt('1');
@@ -52,6 +60,13 @@ class Base32Bench
         $b->reset();
         for ($i = 0; $i < $b->n; $i++) {
             $this->b->decrypt($code);
+        }
+    }
+    public function Decrypt30cm(Benchmark $b)
+    {
+        $data = 'KNSUOVSVLI4W222LGZ2FK5TOG4ZDOZDJNZKEI3CVMN3TSQRVK5YXO6KBKFGECT3NJBWTOODEKFCHIVJZIJNHK4LTOJXU45KCLFNESNTCONGS6RTIGN5GOUZXJF2GIZRZMQ3WKSDZJEYWSODBIV3USOLHIZVGEYLDINYEEVZSMIYVIUTEG54VIRKFOJEXUTJLHFWSWQSNKVCHSOLKOBFEQQTRJ5BXI2L2IFKUOY2FK43XM6CEMVFXCS3BPAVWWRCB';
+        for ($i = 0; $i < $b->n; $i++) {
+            $this->b->decrypt($data);
         }
     }
 }
